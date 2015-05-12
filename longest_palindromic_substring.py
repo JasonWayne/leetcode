993f4e847910f0e587d32d3789ed1967d130b05c
JasonWayne <wuwenjie0102@gmail.com>
2015-05-12 23:57:56 +0800

    accepted longest palindromic substring with dp

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -2,32 +2,30 @@ class Solution:
     # @param {string} s
     # @return {string}
     def longestPalindrome(self, s):
-    	if s is None:
-    		return None
-    	cur_longest_string = None
-    	cur_longest_length = 0
-    	for i in range(len(s) - 1):
-    		for j in range(i + 1, len(s)):
-    			if j - i <= cur_longest_length:
-    				continue
-    			if self.isPalindrome(s[i:j]):
-    				cur_longest_string = s[i:j]
-    				cur_longest_length = j - i
-    	return cur_longest_string
+    	# inspired by https://leetcode.com/discuss/21332/python-o-n-2-method-with-some-optimization-88ms
+    	start = 0
+    	max_length = 1
+    	for i in range(len(s)):
+    		if i - max_length - 1 >= 0 \
+    			and self.isPalindrom(s[i - max_length - 1:i+1]):
+    			start = i - max_length - 1
+    			max_length = max_length + 2
+    			continue
+    		if i - max_length >= 0 \
+    			and self.isPalindrom(s[i - max_length:i+1]):
+    			start = i - max_length
+    			max_length = max_length + 1
+    	return s[start:start+max_length]
 
-    def isPalindrome(self, s):
-		for i in range(len(s) / 2):
-			if s[i] != s[len(s) - 1 - i]:
-				return False
-		return True
+    def isPalindrom(self, s):
+    	max_index = len(s) - 1
+    	for i in range(len(s) / 2):
+    		if s[i] != s[max_index - i]:
+    			return False
+    	return True
 
 
 if __name__ == "__main__":
-	# this is a very naive solution and got TLE error
-	# try use Manacher's algorithm 
-	# http://en.wikipedia.org/wiki/Longest_palindromic_substring
-	# https://leetcode.com/discuss/28791/manacher-algorithm-in-python-o-n
  	solution = Solution()
  	assert solution.longestPalindrome('abacb') == 'aba'
  	assert solution.longestPalindrome('abcbad') == 'abcba'
- 	assert solution.longestPalindrome('') == None
